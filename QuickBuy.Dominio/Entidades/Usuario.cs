2080035a8da9252fcec25f9387bd93c5e8e5120d
rstonon <rstonon@gmail.com>
2022-04-06 16:44:06 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+        /// <summary>
+        /// Um Usuario pode ter nenhum ou vários pedidos
+        /// </summary>
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
