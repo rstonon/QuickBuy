b584f7df40dfc35ef9fcf83677ffff76932eaa08
rstonon <rstonon@gmail.com>
2022-04-07 18:00:49 -0300

    Configuração EF

---
+++ QuickBuy.Repositorio/Contexto/QuickBuyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Contexto
+{
+    public class QuickBuyContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedidos { get; set; }
+        public DbSet<FormaPagamento> FormasPagamento { get; set; }
+    }
+}
